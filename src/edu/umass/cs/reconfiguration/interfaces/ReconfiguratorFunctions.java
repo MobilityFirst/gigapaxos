00b20fa817442ba89e18d302a9c11cf51829bf34
V. Arun <arun@cs.umass.edu>
2016-09-23 11:26:40 -0400

    Adding missing file.

---
+++ src/edu/umass/cs/reconfiguration/interfaces/ReconfiguratorFunctions.java
@@ -0,0 +1,43 @@
+package edu.umass.cs.reconfiguration.interfaces;
+
+import edu.umass.cs.gigapaxos.async.RequestCallbackFuture;
+import edu.umass.cs.gigapaxos.interfaces.Callback;
+import edu.umass.cs.gigapaxos.interfaces.Request;
+import edu.umass.cs.gigapaxos.interfaces.RequestCallback;
+import edu.umass.cs.reconfiguration.Reconfigurator;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.BasicReconfigurationPacket;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.ClientReconfigurationPacket;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.CreateServiceName;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.DeleteServiceName;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.RequestActiveReplicas;
+import edu.umass.cs.reconfiguration.reconfigurationpackets.ServerReconfigurationPacket;
+
+/**
+ * @author arun
+ * 
+ *         A minimal interface defining reconfigurator server functions. This
+ *         interface is implemented by {@link Reconfigurator} and must be
+ *         implemented by any client or proxy for reconfigurators.
+ *
+ */
+public interface ReconfiguratorFunctions {
+
+	/**
+	 * @param request
+	 * @param callback
+	 * 
+	 * @return A {@link RequestCallbackFuture} object that can be used by the
+	 *         caller to retrieve the processed response.
+	 */
+	public RequestCallbackFuture<ReconfiguratorRequest> sendRequest(
+			ReconfiguratorRequest request,
+			Callback<Request, ReconfiguratorRequest> callback);
+
+	/**
+	 * @param request
+	 * @return The response to {@code request} that will have the same type as
+	 *         the request.
+	 */
+	public ReconfiguratorRequest sendRequest(ReconfiguratorRequest request);
+
+}
