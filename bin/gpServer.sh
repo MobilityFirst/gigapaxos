12432fe436446a456ab16094a8885f6e4a8b6fc5
V. Arun <arun@cs.umass.edu>
2016-11-04 17:17:30 -0400

    Debug.

--- bin/gpServer.sh
+++ bin/gpServer.sh
@@ -374,13 +374,18 @@ function get_address_port {
   fi
   # check if interface is local
   ifconfig_found=`type ifconfig 2>/dev/null`
+  if [[ -z $ifconfig_found ]]; then
+    ifconfig_cmd="ip address"
+  fi
+
+echo $ifconfig_cmd 88888888888888888888888888888
 }
 
 # start server if local, else append to non_local list
 function start_server {
   server=$1
   get_address_port $server
-  if [[ $ifconfig_found != "" && `ifconfig|grep $address` != "" ]]; then
+  if [[ $ifconfig_found != "" && `$ifconfig_cmd|grep $address` != "" ]]; then
     if [[ $VERBOSE == 2 ]]; then
       echo "$JAVA $JVMARGS \
         edu.umass.cs.reconfiguration.ReconfigurableNode $server&"
@@ -435,7 +440,7 @@ function stop_servers {
   for i in $servers; do
       get_address_port $i
       KILL_TARGET="ReconfigurableNode .*$i"
-      if [[ ! -z $ifconfig_found && `ifconfig|grep $address` != "" ]]; 
+      if [[ ! -z $ifconfig_found && `$ifconfig_cmd|grep $address` != "" ]]; 
       then
         pid=`ps -ef|grep "$KILL_TARGET"|grep -v grep|\
           awk '{print $2}' 2>/dev/null`
@@ -474,7 +479,7 @@ if [[ ! -z `echo "$@"|grep "clear[ ]*all"` ]]; then
           stop_servers
           for server in $servers; do
             get_address_port $server
-            if [[ ! -z $ifconfig_found && `ifconfig|grep $address` != "" ]];
+            if [[ ! -z $ifconfig_found && `$ifconfig_cmd|grep $address` != "" ]];
             then
               print 3 "$JAVA $JVMARGS  \
                 edu.umass.cs.reconfiguration.ReconfigurableNode \
