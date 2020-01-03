fd1b84632ff69bc8e4ac36b8230b7ec820e95a4e
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-01-03 11:22:07 -0500

    Add tests for HelloRequest

--- src/edu/umass/cs/reconfiguration/reconfigurationpackets/HelloRequest.java
+++ src/edu/umass/cs/reconfiguration/reconfigurationpackets/HelloRequest.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 
 import edu.umass.cs.gigapaxos.interfaces.ClientRequest;
 import edu.umass.cs.nio.interfaces.Stringifiable;
+import edu.umass.cs.nio.nioutils.StringifiableDefault;
 import edu.umass.cs.reconfiguration.ReconfigurationConfig.RC;
 import edu.umass.cs.utils.Config;
 
@@ -63,11 +64,6 @@ public class HelloRequest<NodeIDType> extends BasicReconfigurationPacket<NodeIDT
 		
 		this.requestID = json.getLong(Keys.QID.toString());
 		this.myID = (NodeIDType) json.get(Keys.ID.toString());
-		/*
-		this.myReceiver = MessageNIOTransport.getReceiverAddress(json);
-		this.setSender(MessageNIOTransport.getSenderAddress(json));
-		this.sentTime = json.getLong(Keys.SENT_TIME.toString());
-		*/
 	}
 	
 	public JSONObject toJSONObjectImpl() throws JSONException {
@@ -91,5 +87,23 @@ public class HelloRequest<NodeIDType> extends BasicReconfigurationPacket<NodeIDT
 	@Override
 	public InetSocketAddress getClientAddress() {
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		final int id = 15;
+		HelloRequest<Integer> helloReq = new HelloRequest<Integer>(id);
+		System.out.println(helloReq);		
+		try {
+			assert(helloReq.myID == id);
+			HelloRequest<Integer> helloReq2 = new HelloRequest<Integer>(
+					helloReq.toJSONObject(), new StringifiableDefault<Integer>(id));
+			assert(helloReq2.toString().equals(helloReq.toString()));
+			
+			HelloRequest<Integer> helloReq3 = new HelloRequest<Integer>(
+					new JSONObject(helloReq.toString()), new StringifiableDefault<Integer>(id));
+			assert(helloReq3.toString().equals(helloReq.toString()));
+		} catch (UnknownHostException | JSONException e) {
+			e.printStackTrace();
+		}
 	}
 }
